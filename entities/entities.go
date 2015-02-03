f0de7c1fae658a7780bde084756537cbd4600417
Patrick Lenz <patrick@limited-overload.de>
2015-02-03 14:46:19 +0100

    Added elasticsearch indexing

--- entities/entities.go
+++ entities/entities.go
@@ -1,6 +1,11 @@
 package entities
 
-import "encoding/xml"
+import (
+	"encoding/xml"
+	"time"
+
+	"github.com/belogik/goes"
+)
 
 type AccountResults struct {
 	XMLName  xml.Name  `xml:"EntityResults"`
@@ -48,12 +53,44 @@ type TimeEntryResults struct {
 }
 
 type TimeEntry struct {
-	XMLName      xml.Name `xml:"Entity"`
-	ID           int      `xml:"id"`
-	HoursWorked  float32
-	ResourceID   int
-	ResourceName string
-	TicketID     int
-	Ticket       *Ticket  `xml:"-"`
-	Account      *Account `xml:"-"`
+	XMLName       xml.Name `xml:"Entity"`
+	ID            int      `xml:"id"`
+	HoursWorked   float32
+	ResourceID    int
+	ResourceName  string
+	TicketID      int
+	Ticket        *Ticket    `xml:"-"`
+	Account       *Account   `xml:"-"`
+	StartDateTime customTime `xml:"StartDateTime"`
+}
+
+func (te *TimeEntry) Document(t *Ticket, a *Account) goes.Document {
+	return goes.Document{
+		Index: "autotask",
+		Type:  "timeentry",
+		Fields: map[string]interface{}{
+			"account_name":  a.AccountName,
+			"ticket_number": t.TicketNumber,
+			"title":         t.Title,
+			"resource_name": te.ResourceName,
+			"hours_worked":  te.HoursWorked,
+			"date_time":     te.StartDateTime,
+		},
+	}
+}
+
+type customTime struct {
+	time.Time
+}
+
+func (c *customTime) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	const atForm = "2006-01-02T15:04:05"
+	var v string
+	d.DecodeElement(&v, &start)
+	parse, err := time.Parse(atForm, v)
+	if err != nil {
+		return nil
+	}
+	*c = customTime{parse}
+	return nil
 }
--- main.go
+++ main.go
@@ -4,9 +4,11 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"net/url"
 	"os"
 	"time"
 
+	"github.com/belogik/goes"
 	"github.com/topmedia/timevacuum/entities"
 )
 
@@ -17,7 +19,52 @@ var (
 		"Password for API access")
 	since = flag.String("since", time.Now().Format("2006-01-02"),
 		"Fetch entries since this date (e.g. 2015-01-16)")
+	es_host = flag.String("es_host", "localhost",
+		"Elasticsearch Host")
+	es_port = flag.String("es_port", "9200",
+		"Elasticsearch Port")
+	es_index = flag.String("es_index", "autotask",
+		"Elasticsearch Index Name")
 )
+
+func CreateIndex(c *goes.Connection) {
+	if exists, _ := c.IndicesExist([]string{*es_index}); exists {
+		return
+	}
+
+	mapping := map[string]interface{}{
+		"mappings": map[string]interface{}{
+			"_default_": map[string]interface{}{
+				"properties": map[string]interface{}{
+					"resource_name": map[string]interface{}{
+						"type": "string",
+						"fields": map[string]interface{}{
+							"raw": map[string]interface{}{
+								"index": "not_analyzed",
+								"type":  "string",
+							},
+						},
+					},
+					"account_name": map[string]interface{}{
+						"type": "string",
+						"fields": map[string]interface{}{
+							"raw": map[string]interface{}{
+								"index": "not_analyzed",
+								"type":  "string",
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	_, err := c.CreateIndex(*es_index, mapping)
+
+	if err != nil {
+		log.Fatal("Failed to create index with mapping: %s", err)
+	}
+}
 
 func main() {
 	flag.Parse()
@@ -27,6 +74,8 @@ func main() {
 	}
 
 	api := NewClient(*user, *pass)
+	es := goes.NewConnection(*es_host, *es_port)
+	CreateIndex(es)
 
 	tes := api.FetchTimeEntries(&entities.QueryExpression{Field: "CreateDateTime", Op: "GreaterThan", Value: *since})
 
@@ -41,6 +90,13 @@ func main() {
 		a := api.FetchAccountByID(t.AccountID)
 		tes[k].Ticket = t
 		tes[k].Account = a
+
+		_, err := es.Index(te.Document(t, a), url.Values{})
+
+		if err != nil {
+			log.Fatal("Indexing failed: %s", err)
+		}
+
 		fmt.Printf("%s #%s %s (%s: %v)\n", a.AccountName, t.TicketNumber, t.Title,
 			te.ResourceName, te.HoursWorked)
 	}
